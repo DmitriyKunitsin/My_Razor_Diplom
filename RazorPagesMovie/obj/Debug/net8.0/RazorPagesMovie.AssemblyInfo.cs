42d886c057b74f70e815226e2e9e2fca5cd107a2
DmitriyKunitsin <kunicind1996@gmail.com>
2023-12-25 19:31:14 +0700

    add new class, and fnctional in About

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a79456e3b670ee2d2d789692686688afc17f510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189fdeb99df9c8f7cac8e77a3af8a8e9292ddcdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
